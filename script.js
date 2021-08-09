63d04351a1fb5c6788a2598b86a304352406c3d5
Omkar Mankar <mankar.omkar2310@gmail.com>
2021-08-09 12:06:07 +0530

    Added omnifood project

---
+++ script.js
@@ -0,0 +1,33 @@
+// NAV BUTTON
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+// STIKCY
+
+const seactionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // root where elements appers we obs in viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+
+obs.observe(seactionHeroEl);
